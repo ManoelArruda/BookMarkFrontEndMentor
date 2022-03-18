807336087b04fcd1cc1151de239924a7db0d1ddc
Manoel Arruda <manoelarruda@outlook.com.br>
2022-03-18 20:11:56 -0300

    accordion

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,3 +35,20 @@ function enviar(){
     alert("Contato realizado com sucesso.");
   }
 }
+
+//questoes
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+document.getElementById("accordion").style.borderBottom = "1px solid hsl(229, 8%, 60%)";
